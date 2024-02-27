eade7c933bc93a6a61d5a3f30c31a3bb04f7bc52
Hai <thanhhaitran1203@gmail.com>
2024-02-27 17:23:16 +0700

    RequestMapping

--- src/main/java/com/example/customerspring/controller/CustomerController.java
+++ src/main/java/com/example/customerspring/controller/CustomerController.java
@@ -3,13 +3,14 @@ import java.util.List;
 import com.example.customerspring.model.Customer;
 import com.example.customerspring.service.CustomerService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.awt.*;
 
 @Controller
+@RequestMapping("/customers")
 public class CustomerController {
     CustomerService customerService = new CustomerService();
 //    @GetMapping("/customers")
@@ -23,11 +24,27 @@ public class CustomerController {
 //        request.setAttribute("customers", customers);
 //        return "list";
 //    }
-    @GetMapping("/customers")
+    @GetMapping("")
     public ModelAndView showList() {
         ModelAndView modelAndView = new ModelAndView("list");
         List<Customer> customers = customerService.findAll();
         modelAndView.addObject("customers", customers);
         return modelAndView;
+    }
+    @GetMapping("/{id}")
+    public ModelAndView showInformation(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("info");
+        Customer customer = customerService.findById(id);
+        modelAndView.addObject("customer", customer);
+        return modelAndView;
+    }
+    @PostMapping
+    public String updateCustomer(@RequestParam Long id,
+                                 @RequestParam String name,
+                                 @RequestParam String email,
+                                 @RequestParam String address) {
+        Customer customer = new Customer(Integer.parseInt(String.valueOf(id)), name, email, address);
+        customerService.save(customer);
+        return "redirect:/customers";
     }
 }
--- src/main/java/com/example/customerspring/service/CustomerService.java
+++ src/main/java/com/example/customerspring/service/CustomerService.java
@@ -6,16 +6,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerService {
+    List<Customer> list = new ArrayList<>();
     public CustomerService() {
-    }
-
-    public List<Customer> findAll(){
-        List<Customer> list = new ArrayList<>();
         list.add(new Customer(1,"hai1","hai1","HN"));
         list.add(new Customer(2,"hai2","hai2","HN"));
         list.add(new Customer(3,"hai3","hai3","HN"));
         list.add(new Customer(4,"hai4","hai4","HN"));
         list.add(new Customer(5,"hai5","hai5","HN"));
+    }
+
+    public List<Customer> findAll(){
+
         return list;
+    }
+
+    public Customer findById(Long id) {
+    Customer customer = new Customer();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getId() == id ){
+                customer = list.get(i);
+            }
+        }
+        return customer;
+    }
+    public Customer findById(int id) {
+        Customer customer = new Customer();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getId() == id ){
+                customer = list.get(i);
+            }
+        }
+        return customer;
+    }
+
+    public void save(Customer customer) {
+        Customer c = findById(customer.getId());
+        c.setName(customer.getName());
+        c.setEmail(customer.getEmail());
+        c.setAddress(customer.getAddress());
     }
 }
